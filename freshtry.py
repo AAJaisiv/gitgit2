5e8b43b5e7b3e17a79d0719742c719b1f8605bed
Aryan Abhinav Jaisiv <aryanabhinav01@gmail.com>
2022-05-03 15:13:26 +0530

    First push

---
+++ freshtry.py
@@ -0,0 +1,96 @@
+
+#name = input("Enter username:")
+
+#password = input("Enter p#assword:")#
+
+
+   
+
+
+#def login(name,password,pin):
+    #if name == "Aryan" and password == "1234" and pin == "edu":
+      # print("Hi",name,"you have logged in successfully")
+    #else:
+        #print("Invalid login")
+        
+#login("Aryan","1234")        
+#login("ASS","1234")
+#login(input("Enter password"),input("Enter password"),input("enter your name"))
+
+#def joined(name,design,location="Banglore",CTC=234567):
+   # print("Hi {} your designation is {} and your work location is {} with CTC Rs.{}".format(name,design,location,CTC))
+        
+
+#joined("Aryan","Chief Executive office","USA")
+#joined("AJA","Chief finance executive")
+#joined(design="CEO",location="USA",name="AJ",CTC="1000000")
+
+
+
+#print(help('modules'))
+
+
+
+     
+#import newtry
+#print(newtry.name)
+#print(newtry.email)
+#rint(newtry.add(34,33))
+
+#from newtry import *
+
+#from anothertry import *
+
+
+#import newtry
+#import anothertry
+#print(newtry.name)
+#print(anothertry.name)
+
+#print(anothertry.mul(23,44))
+#print(newtry.mul(34,22))
+
+#def fact_cal(a):
+ #   if a==0 or a==1:
+   #  return 1
+  #  else:
+  #    return a*fact_cal(a-1)
+  
+#print(fact_cal(9))
+            
+
+
+#print(help('modules'))
+
+##a=34
+#b=55#
+#print("sum=",(a+b))        
+
+#import random 
+#name= input("Enter your name:")
+#amount= random.randint(1000,999999)
+
+#print("Hi {} your otp is {} for the transaction".format(name,amount))
+
+#now_date = datetime.now()
+#print(now_date)
+#print(type(now_date))
+##str_date = now_date.strftime('%y-%B-%d %H:%M %A')
+#print(str_date)
+
+#str_date = "18 December 2021"
+#print(str_date)
+#datetime_str = datetime.strftime(str_date,"%d %B %Y")
+#print(datetime_str)
+#print(type(datetime_str))
+
+
+a=45
+b=56
+c= a+b
+
+print("sum=",c)
+
+exit()
+
+    
