7fbdfdce297e85ed88564a047df948de0ccf8637
Aryan Abhinav Jaisiv <aryanabhinav01@gmail.com>
2022-05-03 16:47:28 +0530

    updated

--- developer1.py
+++ developer1.py
@@ -3,7 +3,7 @@ passowrd = input("Enter your password:")
 
 def login(name,password):
     if name == "Aryan" and password == "123":
-       print("HI",name, "you have logged in successfully") 
+       print("HI {} you have logged in successfully".format(name)) 
     else:
         print("Invalid login")   
 login("Aryan","123")
